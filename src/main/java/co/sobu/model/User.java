63bdc1a82eb50bbd483461a12da6221ad7452063
AServantDev <arnaud.github@gmail.com>
2018-12-20 12:25:53 +0100

    Ajout de la javadoc auto et manuscrite sur la classe User

--- src/main/java/co/sobu/model/User.java
+++ src/main/java/co/sobu/model/User.java
@@ -11,23 +11,71 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import co.sobu.controller.*;
+import co.sobu.service.impl.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+/**
+ * <b>User est la classe représentant un membre de l'API.</b>
+ * <p>
+ * Un User est caractérisé par les informations suivantes :
+ * </p>
+ * <ul>
+ * <li>Un identifiant unique attribué définitivement.</li>
+ * <li>Un pseudo, susceptible d'être changé.</li>
+ * <li>Un nom</li>
+ * <li>Un prénom</li>
+ * <li>Sa taille</li>
+ * <li>Son poids.</li>
+ * <li>Son genre</li>
+ * <li>Son email</li>
+ * <li>Son mot de passe</li>
+ * <li>Son coefficient sportif, qui représente son taux d'activité par semaine, il a plusieurs valeurs: 1.2, 1.375, 1.55, 1.725 et 1.9.</li>  
+ * <li>Des différents programmes qu'il/elle peut suivre aux nombres de 4 : Cétogène, Paléo, Prise de masse et Sèche.</li>
+ * <li>Des macros nutriments qu'il/elle doit ingérer pour garder son poids de forme: Les Kcal, les proteines, les lipides, les glucides. ces apports sont calculés automatiquement</li>
+ * <li>Un prénom</li>
+ * </ul>
+ * 
+ * 	
+ * @see BuildProgram
+ * @see ShredProgram
+ * @see UserServiceImpl
+ * 
+ * @author AServantDev
+ */
 
 @Entity
 @Table(name = "User")
 public class User{
+	
+	/**
+    * L'ID du User. Cet ID n'est pas modifiable.
+    * 
+    * @see User#getId()
+    */
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column
 	private int id;
 
+	/**
+	 * Le programme de sèche du User, l'attribut est null à la création du user
+	 * il ne peux s'inscrire qu'à un seul programme de sèche.
+	 * @see ProgramController#saveShred
+	 */
 	
 	@OneToOne(cascade=CascadeType.REMOVE)
 	@JoinColumn(name="idShred")
 	private ShredProgram shred;
 
+	/**
+	 * Le programme de prise de masse du User, l'attribut est null à la création du user
+	 * il ne peux s'inscrire qu'à un seul programme de prise de masse.
+	 * @see ProgramController#saveBuild
+	 */
 	
 	@OneToOne(cascade=CascadeType.REMOVE)
 	@JoinColumn(name="idBuild")
@@ -40,31 +88,115 @@ public class User{
 	
 	@OneToOne(mappedBy = "paleoUser", cascade=CascadeType.REMOVE)
 	private PaleoDiet paleo;
+	
+	/**
+	 * Le prénom du user ,l'attribut est défini à la création du user
+	 * @see UserController#saveUser
+	 */
 	@Column
 	private String name;
+	
+	/**
+	 * Le nom de famille du user ,l'attribut est défini à la création du user. 
+	 */
 	@Column
 	private String lastname;
+	
+	/**
+	 * L'age du user ,l'attribut est défini à la création du user, il servira à calculer les macros nutriments
+	 * @see UserController#saveUser
+	 */
 	@Column
 	private int age;
+	
+	/**
+	 * La taille du user ,l'attribut est défini à la création du user, il servira à calculer les macros nutriments
+	 * @see UserController#saveUser
+	 */
 	@Column
 	private double heigth;
+	
+	/**
+	 * Le poids du user ,l'attribut est défini à la création du user, il servira à calculer les macros nutriments
+	 * @see UserController#saveUser
+	 */
 	@Column
 	private double weight;
 
+	
+	/**
+	 * Le mail du user ,l'attribut est défini à la création du user, il est unique donc deux Users ne peuvent s'inscrire avec le même mail.
+	 * @see UserController#saveUser
+	 */
 	@Column(unique = true)
 	private String email;
 
+	/**
+	 * Le mot de passe du user ,l'attribut est défini à la création du user, le mot de passe est chiffré à l'enregistrement.
+	 * @see UserController#saveUser
+	 * @see BCryptPasswordEncoder
+	 */
 	private String password;
+	
+	/**
+	 * Les kilos calories qu'un user doit ingérer pour garder son poids de forme, elle sont calculé a la création du User en fonction d'autres attributs.
+	 * @see User#age
+	 * @see User#gender
+	 * @see User#heigth
+	 * @see User#weight
+	 * @see User#coefSportif
+	 * @see UserController#saveUser
+	 */
 	@Column
 	private double KcalPerDay;
+	
+	/**
+	 * Les proteines qu'un user doit ingérer pour garder son poids de forme, elle sont calculé a la création du User en fonction d'autres attributs.
+	 * @see User#age
+	 * @see User#gender
+	 * @see User#heigth
+	 * @see User#weight
+	 * @see User#coefSportif
+	 * @see UserController#saveUser
+	 */
 	@Column
 	private double ProtPerDay;
+	
+	/**
+	 * Les lipides calories qu'un user doit ingérer pour garder son poids de forme, elle sont calculé a la création du User en fonction d'autres attributs.
+	 * @see User#age
+	 * @see User#gender
+	 * @see User#heigth
+	 * @see User#weight
+	 * @see User#coefSportif
+	 * @see UserController#saveUser
+	 */
 	@Column
 	private double FatsPerDay;
+	
+	/**
+	 * Les glucides calories qu'un user doit ingérer pour garder son poids de forme, elle sont calculé a la création du User en fonction d'autres attributs.
+	 * @see User#age
+	 * @see User#gender
+	 * @see User#heigth
+	 * @see User#weight
+	 * @see User#coefSportif
+	 * @see UserController#saveUser
+	 */
 	@Column
 	private double CarbsPerDays;
+	
+	/**
+	 * Le genre du user ,l'attribut est défini à la création du user, il servira à calculer les macros nutriments
+	 * @see UserController#saveUser
+	 */
 	@Column
 	private String gender;
+	
+	/**
+	 * Le coefficient sportif du user, il est calculé en fonction de l'activité sprotive d'un user ,l'attribut est défini à la création du user, il servira à calculer les macros nutriments
+	 * @see UserController#saveUser
+	 */
 	@Column
 	private double coefSportif;
 	
